e0ba2e8009c8be0bf29777b00a5b2e4f85eb9176
nkgwer <yota.nakagawa@suou.waseda.jp>
2016-11-12 20:14:19 +0900

    updated

--- function.py
+++ function.py
@@ -2,8 +2,10 @@ import math
 
 def ceiling(x,p=0):
 	if (p==-1):
+		#starting point of the period
 		return -1*math.pi
 	if (p==1):
+		#ending point of the period
 		return 1*math.pi
 	return math.ceil(5*(x%math.pi))/5
 
@@ -16,7 +18,11 @@ def square(x,p=0):
 		return 1
 	else:
 		return -1
-def expwave(x):
+def expwave(x,p=0):
+	if (p==-1):
+		return -1*math.pi
+	if (p==1):
+		return 1*math.pi
 	if(x%(2*math.pi)<math.pi):
 		return math.exp(x%math.pi)
 	else:
